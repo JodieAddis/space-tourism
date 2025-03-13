6a80cab4897ac13cdd8f2cb34203ad7eefd9c75c
JodieAddis <jodie.addis.92@gmail.com>
2025-03-13 15:02:31 +0100

    Onglet fonctionnel pour l'affiche des data sur la page crew

--- src/components/CrewContainer.tsx
+++ src/components/CrewContainer.tsx
@@ -2,15 +2,27 @@ import { crewData } from "../data/crew";
 import { useState } from "react";
 
 const CrewContainer = () => {
-  const [selectedPeople, setSelectedPeople] =
-    useState<string>("Douglas Hurley");
+  const [selectedTab, setSelectedTab] = useState<string>("Douglas Hurley");
 
-  const handleViewSelect = (tab: string) => {
-    setSelectedPeople(tab);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const people = e.currentTarget.dataset.crew as string;
+    console.log("personne sélectionnée:", people);
+    // les attributs data-* sont en fait récupérés en tant que string | undefined (pas un string strict), donc on doit vérifier le type d'abord
+    if (typeof people === "string") {
+      setSelectedTab(people);
+      console.log("Personne sélectionnée:", people);
+    } else {
+      console.log("Aucune personne sélectionnée ou data-crew non défini");
+    }
   };
+
+  const selectedPeople = crewData.find((people) => people.name === selectedTab);
+  if (!selectedPeople) {
+    return <p>Error: data not found</p>;
+  }
   return (
     <>
-      {crewData.map((data) => (
+      {selectedTab === selectedPeople.name && (
         <div
           id="container"
           className="flex flex-col lg:flex-row gap-8 lg:w-full lg:h-full mt-8 lg:mt-0"
@@ -24,25 +36,39 @@ const CrewContainer = () => {
               className={`text-center lg:text-left`}
             >
               <h2 className="text-base lg:text-3xl uppercase font-Bellefair opacity-50 mt-8 lg:mt-0">
-                {data.role}
+                {selectedPeople.role}
               </h2>
               <h3 className="text-2xl lg:text-5xl uppercase font-Bellefair mt-3 lg:mt-8">
-                {data.name}
+                {selectedPeople.name}
               </h3>
               <p className="text-base lg:text-lg font-Barlow mt-6 lg:mt-12 mx-3 lg:mx-0 lg:w-[444px]">
-                {data.bio}
+                {selectedPeople.bio}
               </p>
             </div>
             <div
               id="info-container-tab"
               className={` w-full flex justify-center lg:justify-start mt-8 lg:mt-36`}
             >
-              <slot name="tab"></slot>
-              {/*slot pour intégrer les différents onglets */}
-              <span slot="tab" className="crew-tab"></span>
-              <span slot="tab" className="crew-tab"></span>
-              <span slot="tab" className="crew-tab"></span>
-              <span slot="tab" className="crew-tab"></span>
+              <button
+                data-crew="Douglas Hurley"
+                className="crew-tab"
+                onClick={handleClick}
+              ></button>
+              <button
+                data-crew="Mark Shuttleworth"
+                className="crew-tab"
+                onClick={handleClick}
+              ></button>
+              <button
+                data-crew="Victor Glover"
+                className="crew-tab"
+                onClick={handleClick}
+              ></button>
+              <button
+                data-crew="Anousheh Ansari"
+                className="crew-tab"
+                onClick={handleClick}
+              ></button>
             </div>
           </div>
           <div
@@ -50,14 +76,14 @@ const CrewContainer = () => {
             className="flex flex-col items-center order-1 lg:order-2"
           >
             <img
-              src={data.images}
-              alt={`Picture of ${data.name}`}
+              src={selectedPeople.images}
+              alt={`Picture of ${selectedPeople.name}`}
               className="w-1/2 lg:w-full lg:max-w-md lg:h-auto"
             />
             <hr className="bg-white h-[1px] w-5/6 opacity-50 lg:hidden" />
           </div>
         </div>
-      ))}
+      )}
     </>
   );
 };
